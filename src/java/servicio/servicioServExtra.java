b3dd14e27c94c7e09269ed9cc6c4f9e6e4364ba7
other <other@DESKTOP-P0ES69G>
2018-06-30 07:12:01 -0300

    se agregan cambios al servicio

--- src/java/servicio/servicioServExtra.java
+++ src/java/servicio/servicioServExtra.java
@@ -19,8 +19,41 @@ public class servicioServExtra {
     /**
      * This is a sample web service operation
      */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+    
+    @WebMethod(operationName = "reservarServicio")
+    public int reservarServicio(@WebParam(name = "idServicio") String idServicio) {
+        
+        int retorno;
+        
+        /*
+            21	Cancha de tennis
+            22	Spa
+            0	sin servicio
+            20	Piscina
+            19	Gimnasio
+        */
+        
+        switch (idServicio){
+            case "21":   
+                    retorno = 1;
+                     break;
+            case "22": 
+                    retorno = 0;
+                     break;
+            case "0": 
+                    retorno = 0;
+                     break;
+            case "20": 
+                    retorno = 1;
+                     break;
+            case "19": 
+                    retorno = 1;
+                     break;         
+            default:
+                    retorno = 1;
+                     break;
+        }
+        
+        return retorno;
     }
 }
